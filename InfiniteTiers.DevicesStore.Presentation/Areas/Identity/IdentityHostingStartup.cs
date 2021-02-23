91fdb09c28d1cd586ad9aeb5442b08f67a8129f2
Nael-AK <naels141@gmail.com>
2021-02-23 16:52:42 +0200

    Add register functionality

--- InfiniteTiers.DevicesStore.Presentation/Areas/Identity/IdentityHostingStartup.cs
+++ InfiniteTiers.DevicesStore.Presentation/Areas/Identity/IdentityHostingStartup.cs
@@ -19,7 +19,7 @@ namespace InfiniteTiers.DevicesStore.Presentation.Areas.Identity
                     options.UseSqlServer(
                         context.Configuration.GetConnectionString("AuthDbContextConnection")));
 
-                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
                     .AddEntityFrameworkStores<AuthDbContext>();
             });
         }
