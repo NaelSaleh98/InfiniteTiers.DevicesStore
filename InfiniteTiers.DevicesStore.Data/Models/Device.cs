fc4333d5d318b36c81cc7a6f454ca32f04a9a202
Nael-AK <naels141@gmail.com>
2021-03-18 13:15:59 +0200

    Add Device validation

--- InfiniteTiers.DevicesStore.Data/Models/Device.cs
+++ InfiniteTiers.DevicesStore.Data/Models/Device.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace InfiniteTiers.DevicesStore.Data.Models
 {
@@ -6,19 +7,32 @@ namespace InfiniteTiers.DevicesStore.Data.Models
     {
         public int DeviceId { get; set; }
 
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Name { get; set; }
+
+        [StringLength(120)]
         public string Description { get; set; }
+
+        [StringLength(30)]
+        [Required]
         public string Manufacturer { get; set; }
+
+        [StringLength(60)]
         public string Model { get; set; }
 
+        [StringLength(120)]
+        [Required]
         public string SerialNumber { get; set; }
-        public DateTime PurchaseDate { get; set; }
-        public bool IsActive { get; set; }
 
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Purchase Date")]
+        public DateTime PurchaseDate { get; set; }
+
+        public bool IsActive { get; set; }
         public int CategoryId { get; set; }
         public Category Category { get; set; }
         public string ApplicationUserId { get; set; }
-
         public string OwnedBy { get; set; }
     }
 }
